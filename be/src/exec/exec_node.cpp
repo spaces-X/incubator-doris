de8ba226a7276a0e217e942dd7808a2c6bdcd44c
jacktengg <tengjp@outlook.com>
2022-03-07 22:35:51 +0800

    stream load vectorization: vectorized broker scan and tablet sink

--- be/src/exec/exec_node.cpp
+++ be/src/exec/exec_node.cpp
@@ -78,6 +78,7 @@
 #include "vec/exec/vselect_node.h"
 #include "vec/exec/vsort_node.h"
 #include "vec/exec/vunion_node.h"
+#include "vec/exec/vbroker_scan_node.h"
 #include "vec/exprs/vexpr.h"
 namespace doris {
 
@@ -387,6 +388,7 @@ Status ExecNode::create_node(RuntimeState* state, ObjectPool* pool, const TPlanN
         case TPlanNodeType::ANALYTIC_EVAL_NODE:
         case TPlanNodeType::SELECT_NODE:
         case TPlanNodeType::REPEAT_NODE:
+        case TPlanNodeType::BROKER_SCAN_NODE:
             break;
         default: {
             const auto& i = _TPlanNodeType_VALUES_TO_NAMES.find(tnode.node_type);
@@ -550,7 +552,11 @@ Status ExecNode::create_node(RuntimeState* state, ObjectPool* pool, const TPlanN
         return Status::OK();
 
     case TPlanNodeType::BROKER_SCAN_NODE:
-        *node = pool->add(new BrokerScanNode(pool, tnode, descs));
+        if (state->enable_vectorized_exec()) {
+            *node = pool->add(new vectorized::VBrokerScanNode(pool, tnode, descs));
+        } else {
+            *node = pool->add(new BrokerScanNode(pool, tnode, descs));
+        }
         return Status::OK();
 
     case TPlanNodeType::REPEAT_NODE:
